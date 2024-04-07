0fba5410faf5d2285dd9af527f58cde5b6fc0443
SebOra-WSEI <LiBroPWSZ@gmail.com>
2024-04-07 20:00:02 +0200

    Clean up

--- typescript-project/src/components/Project/Details/ProjectDetailsPage.tsx
+++ typescript-project/src/components/Project/Details/ProjectDetailsPage.tsx
@@ -5,23 +5,9 @@ import { Loader } from '../../common/Loader';
 import { Navbar } from '../../common/Navbar';
 import { StorageListView } from '../../Storage/List/StorageListView';
 import { useGetStoragesByProjectId } from '../../../api/storage/useGetStoragesByProjectId';
-import { Storage } from '../../../controllers/storage';
-import { Priority } from '../../../types/priority';
-import { State } from '../../../types/state';
 
 export const ProjectDetailsPage: React.FC = () => {
   const { projectId } = useParams<{ projectId: string }>();
-
-  // const EMPTY_STORAGE1 = new Storage(
-  //   'one new storage DONE 1',
-  //   Priority.Low,
-  //   projectId,
-  //   'fake',
-  //   State.Done,
-  //   'fake'
-  // );
-
-  // console.log(EMPTY_STORAGE1.create())
 
   const {
     loading: projectLoading,
--- typescript-project/src/utils/localStorage.ts
+++ typescript-project/src/utils/localStorage.ts
@@ -5,5 +5,3 @@ export function getFromLocalStorage(key: string): string {
 export function setToLocalStorage(key: string, element: string): void {
   window.localStorage.setItem(key, element);
 }
-
-export const SELECTED_PROJECT = 'selectedProject';
