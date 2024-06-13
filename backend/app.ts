2c3be4e047a7f796a4ec56f0449eeddbc2683693
SebOra-WSEI <LiBroPWSZ@gmail.com>
2024-06-13 11:46:17 +0200

    refactor

--- backend/app.ts
+++ backend/app.ts
@@ -9,7 +9,7 @@ import { signIn } from './handlers/signIn';
 import { user } from './handlers/user';
 import { project } from './handlers/project';
 import { story } from './handlers/story';
-import { QueryResponse } from './types/queryResponse';
+import { createHandlerFunction } from './utils/createHandlerFunction';
 
 const app = express();
 const port = '3000';
@@ -17,65 +17,6 @@ let refreshTokenJwt: string = '';
 
 app.use(cors());
 app.use(express.json());
-
-type ApiHandlerFnc = (req: Request, res: Response) => Promise<void>;
-
-interface XX<T, B> {
-  getAll?: (parentId?: string) => Promise<QueryResponse<T>>;
-  getById?: (id: string) => Promise<QueryResponse<T>>;
-  create?: (body: B) => Promise<QueryResponse<T>>;
-  remove?: (id: string) => Promise<QueryResponse<T>>;
-  update?: (id: string, body: B) => Promise<QueryResponse<T>>;
-}
-
-function xx<T, B>(functions: XX<T, B>): ApiHandlerFnc {
-  return async (req: Request, res: Response) => {
-    const id = req.params?.id;
-    const projectId = req.query.projectId as string;
-    const authHeader = req.headers.authorization;
-    const token = authHeader?.split(' ')[1] ?? '';
-
-    if (getTokenError(token).length) {
-      const response: ResponseField<undefined> = {
-        error: 'Token is not provided',
-        data: undefined,
-      };
-
-      res.status(StatusCode.BadRequest).send(response);
-      return;
-    }
-
-    if (functions?.getAll) {
-      const { status, response } = await functions.getAll(projectId);
-      res.status(status).send(response);
-      return;
-    }
-
-    if (functions?.getById) {
-      const { status, response } = await functions.getById(id);
-      res.status(status).send(response);
-      return;
-    }
-
-    if (functions?.create) {
-      const { status, response } = await functions.create(req.body);
-      res.status(status).send(response);
-      return;
-    }
-
-    if (functions?.remove) {
-      const { status, response } = await functions.remove(id);
-      res.status(status).send(response);
-      return;
-    }
-
-    if (functions?.update) {
-      const { status, response } = await functions.update(id, req.body);
-      res.status(status).send(response);
-      return;
-    }
-  };
-}
 
 // Login user
 app.post('/sign-in', async (req: Request, res: Response) => {
@@ -115,19 +56,19 @@ app.post('/refresh-token', async (req: Request, res: Response) => {
   res.status(status).send(response);
 });
 
-app.get('/users', xx({ getAll: user.getAll }));
+app.get('/users', createHandlerFunction({ getAll: user.getAll }));
 
-app.get('/projects', xx({ getAll: project.getAll }));
-app.get('/projects/:id', xx({ getById: project.getById }));
-app.post('/projects', xx({ create: project.create }));
-app.put('/projects/:id', xx({ update: project.update }));
-app.delete('/projects/:id', xx({ remove: project.remove }));
+app.get('/projects', createHandlerFunction({ getAll: project.getAll }));
+app.get('/projects/:id', createHandlerFunction({ getById: project.getById }));
+app.post('/projects', createHandlerFunction({ create: project.create }));
+app.put('/projects/:id', createHandlerFunction({ update: project.update }));
+app.delete('/projects/:id', createHandlerFunction({ remove: project.remove }));
 
-app.get('/stories', xx({ getAll: story.getAll }));
-app.get('/stories/:id', xx({ getById: story.getById }));
-app.post('/stories', xx({ create: story.create }));
-app.delete('/stories/:id', xx({ remove: story.remove }));
-app.put('/stories/:id', xx({ update: story.update }));
+app.get('/stories', createHandlerFunction({ getAll: story.getAll }));
+app.get('/stories/:id', createHandlerFunction({ getById: story.getById }));
+app.post('/stories', createHandlerFunction({ create: story.create }));
+app.delete('/stories/:id', createHandlerFunction({ remove: story.remove }));
+app.put('/stories/:id', createHandlerFunction({ update: story.update }));
 
 app.listen(port, () => {
   console.log(`🚀 Server running on port ${port}`);
