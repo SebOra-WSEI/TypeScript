9a4da5b3c6066e2cebdcd8c12b154a1f8c272d5f
SebOra-WSEI <LiBroPWSZ@gmail.com>
2024-06-19 18:08:55 +0200

    remove

--- typescript-project/src/components/Task/Form/Create/CreateTaskFormModal.tsx
+++ typescript-project/src/components/Task/Form/Create/CreateTaskFormModal.tsx
@@ -2,8 +2,22 @@ import React, { useState } from 'react';
 import { CreateTaskForm } from './CreateTaskForm';
 import { TaskBasic } from '../../../../types/task';
 import { useCreateTask } from '../../../../queries/task/useCreateTask';
-import { defaultTask } from '../../../../queries/task/task';
 import { ModalContent } from '../../../common/ModalContent';
+import { Priority } from '../../../../types/priority';
+import { State } from '../../../../types/state';
+
+const defaultTask: TaskBasic = {
+  name: '',
+  description: '',
+  priority: Priority.High,
+  state: State.Todo,
+  createdDate: String(new Date().getTime()),
+  startDate: undefined,
+  expectedEndTime: String(new Date().getTime()),
+  endDate: undefined,
+  storyPoint: 1,
+  storyId: '',
+};
 
 interface CreateTaskFormModalProps {
   isOpen: boolean;
--- typescript-project/src/queries/task/task.ts
+++ typescript-project/src/queries/task/task.ts
@@ -1,18 +1,6 @@
-import { Task } from '../../controllers/task';
 import { Priority } from '../../types/priority';
 import { State } from '../../types/state';
 import { TaskBasic } from '../../types/task';
-
-export const EMPTY_TASK = new Task(
-  '',
-  '',
-  '',
-  Priority.High,
-  State.Todo,
-  new Date(),
-  0,
-  ''
-);
 
 export const defaultTask: TaskBasic = {
   name: '',
