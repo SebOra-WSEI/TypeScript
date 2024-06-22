a714aabe8c895885bfb57e754fb4c1619af487e1
SebOra-WSEI <LiBroPWSZ@gmail.com>
2024-06-22 14:26:40 +0200

    add cascading remove

--- backend/api/project/removeProject.ts
+++ backend/api/project/removeProject.ts
@@ -1,6 +1,19 @@
 import { DB } from '../../connection/connection';
+import { getAllStories } from '../story/getAllStories';
+import { removeStory } from '../story/removeStory';
+import { getAllTasks } from '../task/getAllTasks';
+import { removeTask } from '../task/removeTask';
 
 export async function removeProject(id: string): Promise<boolean> {
+  const stories = await getAllStories(id);
+
+  await stories?.forEach(async ({ id }) => {
+    const tasks = await getAllTasks(String(id));
+    tasks?.forEach(async ({ id }) => await removeTask(String(id)));
+
+    await removeStory(String(id));
+  });
+
   return await new Promise((resolve) => {
     DB.query(`DELETE FROM projects WHERE id = (?)`, [id], (err, res) => {
       resolve(res);
--- backend/api/story/removeStory.ts
+++ backend/api/story/removeStory.ts
@@ -1,9 +1,12 @@
 import { DB } from '../../connection/connection';
-import { getAllStories } from './getAllStories';
+import { getAllTasks } from '../task/getAllTasks';
+import { removeTask } from '../task/removeTask';
 
 export async function removeStory(id: string): Promise<boolean> {
-  const tasks = await getAllStories(id);
-  // console.log({ tasks });
+  const tasks = await getAllTasks(id);
+  tasks?.forEach(async ({ id }) => {
+    await removeTask(String(id));
+  });
 
   return await new Promise((resolve) => {
     DB.query(`DELETE FROM stories WHERE id = (?)`, [id], (err, res) => {
